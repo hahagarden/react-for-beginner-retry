4988cf1c0bb972001e3cb245ca4af7105b95ef60
hahagarden <tingyuan413@gmail.com>
2022-09-18 17:08:39 +0900

    ~.module.css
    * Create React App offer this work!
    good for Divide and Conquer.
    컴포넌트별 css파일을 만들고(모듈화) 스타일을 적용할 수 있음
    컴포넌트별 css파일이기 때문에 클래스네임 다른곳에서도 사용 가능
    CRA가 우리가 만든 클래스네임에 따로 랜덤아이디를 붙임
    Button.module.css에서 .name으로 스타일링하고
    App.module.css에서 .name으로 스타일링 가능.
    클래스명을 다~ 다르게 하지 않아도됨.

--- cra-react-app/src/App.js
+++ cra-react-app/src/App.js
@@ -1,8 +1,14 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
+      <h1 className={styles.title}>
+        ~.module.css: <br />
+        CRA turns this css code into javascript object, <br />
+        and Component import this object that has .className
+      </h1>
       <Button text={"hello"} />
     </div>
   );
